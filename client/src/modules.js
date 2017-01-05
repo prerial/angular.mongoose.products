aa9466737f43ec62d7f4c1b25fa7881d09aba42f
Mikhail Grinshteyn <mgrinsht@gmail.com>
2017-01-05 12:39:35 -0500

    Add checkout

--- client/src/modules.js
+++ client/src/modules.js
@@ -4,6 +4,22 @@
     angular.module('sportsStore', ['ngRoute', 'ngResource', 'customFilters', 'cart'])
         .constant("productListActiveClass", "btn-primary")
         .constant("dataUrl", "/products")
-        .constant("productListPageCount", 3);
-
+        .constant("productListPageCount", 3)
+        .config(function ($routeProvider) {
+            $routeProvider.when("/complete", {
+                templateUrl: "/views/thankYou.html"
+            });
+            $routeProvider.when("/placeorder", {
+                templateUrl: "/views/placeOrder.html"
+            });
+            $routeProvider.when("/checkout", {
+                templateUrl: "/views/checkoutSummary.html"
+            });
+            $routeProvider.when("/products", {
+                templateUrl: "/views/productList.html"
+            });
+            $routeProvider.otherwise({
+                templateUrl: "/views/productList.html"
+            });
+    });
 })();
